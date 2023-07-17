eb5b2488d87c7575f54a5b870539a607323cf63d
ThanhHienHoang <thanhhien.hoang272@gmail.com>
2023-07-17 22:51:24 +0700

    refactor: update information

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,11 +1,18 @@
-class Page{
-  constructor(){
-const root=this;
-root._initAge();
+class Page {
+  constructor() {
+    const root = this;
+    root._initAge();
+    root._initDownloadCVBtn();
   }
-  _initAge(){
-    $(".user-age").text(new Date().getFullYear()-1998);
-    $(".page-year").text(new Date().getFullYear()-1);
+  _initAge() {
+    $(".user-age").text(new Date().getFullYear() - 1998);
+    $(".page-year").text(new Date().getFullYear() - 1);
+  }
+  _initDownloadCVBtn() {
+    $(".download-cv").on("click", (e) => {
+      e.preventDefault();
+      window.open('./assets/files/CV_HoangThiThanhHien_UIUXDesigner.pdf', '_blank');
+    });
   }
 }
 
